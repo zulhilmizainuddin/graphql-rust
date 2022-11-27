8ec6bae7fa0b1734a02eb37c198196ede2e0906b
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2022-11-27 14:31:26 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Result};
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema, Object, SimpleObject};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+
+#[derive(SimpleObject)]
+struct Book {
+    title: &'static str,
+    author: &'static str,
+}
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn name(&self) -> &str {
+        "Zulhilmi"
+    }
+
+    async fn book(&self) -> Book {
+        Book { title: "Lord of the Rings", author: "Tolkien" }
+    }
+}
+
+type MainSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+
+async fn index(schema: web::Data<MainSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_graphiql() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(
+            GraphiQLSource::build()
+                .endpoint("http://localhost:8000")
+                .finish(),
+        ))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        // .data()
+        .finish();
+
+        println!("GraphiQL IDE: http://localhost:8000");
+
+        HttpServer::new(move || {
+            App::new()
+                .app_data(Data::new(schema.clone()))
+                .service(web::resource("/").guard(guard::Post()).to(index))
+                .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+        })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
